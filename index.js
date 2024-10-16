a242685fd7850ae24373634ec97c7758bccc2621
ShafeeqAhamedS <shafeeqahamed2804@gmail.com>
2024-10-16 13:25:10 +0530

    Update v2

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.get('/authors', function (req, res) {
 });
 
 app.get('/greetings', function (req, res) {
-    res.send('{ "response": "Welcome, all" }');
+    res.send('{ "response": ", all" }');
 });
 
 app.get('/ready', function (req, res) {
